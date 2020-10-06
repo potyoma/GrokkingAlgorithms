18027cf37ea156726bf6d1a4e0f61424611ba4cc
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-10-06 14:54:29 +0300

    implemented binary search

--- Algorithms/Class1.cs
+++ Algorithms/Class1.cs
@@ -1,8 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Algorithms
 {
-    public class Class1
+    public static class BinarySearch
     {
+        /// <summary>
+        /// The method returns index of given item in array. Returns -1 if not found.
+        /// </summary>
+        /// <param name="array">Array to search.</param>
+        /// <param name="item">Object needed to find.</param>
+        /// <typeparam name="T">Any type that extends IComparable.</typeparam>
+        /// <returns>Integer containing index of item in array.</returns>
+        public static int Search<T>(T[] array, T item) where T : IComparable<T>
+        {
+            int low = 0;
+            int high = array.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                T guess = array[mid];
+                if (guess.CompareTo(item) == 0)
+                {
+                    return mid;
+                }
+                if (guess.CompareTo(item) > 0)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
     }
 }
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Algorithms;
+using static System.Console;
 
-namespace GrokkingAlgorithms
+// TODO: Implement xUnit testing instead of Console.Writeline!
+
+namespace Testing
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestBinarySearch();
+        }
+
+        static void TestBinarySearch()
+        {
+            int[] test1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] test2 = { "ant", "beer", "cat", "moon", "wolf" };
+            char[] test3 = { 'a', 'b', 'c', 'd', 'e', 'f' };
+
+            WriteLine(
+                $"Right:     {Array.IndexOf(test1, 5)}\t{Array.IndexOf(test2, "cat")}\t{Array.IndexOf(test3, 'e')}");
+            WriteLine(
+                $"Algorithm: {BinarySearch.Search(test1, 5)}\t{BinarySearch.Search(test2, "cat")}\t{BinarySearch.Search(test3, 'e')}");
         }
     }
 }
